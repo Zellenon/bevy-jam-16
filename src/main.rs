b04f661c49b4bf4528faba5c0c323106d015f380
Benjamin <benhasanemail@gmail.com>
2025-06-07 14:48:47 -0400

    FUCKING CLIPPY

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-// Support configuring Bevy lints within code.
-#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
-// Disable console on Windows for non-dev builds.
-#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
-// TEMP - allow these to unblock builds
 #![allow(dead_code)]
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 #![allow(unused_parens)]
+// Support configuring Bevy lints within code.
+#![cfg_attr(bevy_lint, feature(register_tool), register_tool(bevy))]
+// Disable console on Windows for non-dev builds.
+#![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
+
 
 use bevy::prelude::*;
 use chain_reaction_towers::AppPlugin;
